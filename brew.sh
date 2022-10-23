10078f12e668fb0a7d153eee8bf66eb721e3a530
ihidzhov <93243665+ihidzhov@users.noreply.github.com>
2022-10-23 16:24:18 +0300

    Brew sh

---
+++ brew.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+brew update
+
+brew upgrade
+
+brew install bash
+
+brew install wget --with-iri
+
+brew install openssh
+
+brew install screen
+
+brew install php
+
+brew install git
+
+brew cleanup
